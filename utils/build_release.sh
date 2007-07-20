b8e8587b2c379fe285c27ed1253c7e3d50cc7733
edyfox <edyfox@1bbdc759-f934-0410-94f1-a90c52dc07f7>
2007-07-20 08:07:11 +0000

    r62@lifu:  lifu | 2007-07-20 15:58:24 +0800  Fixed the build path into the build directory. Forcing off-directory build.

--- utils/build_release.sh
+++ utils/build_release.sh
@@ -6,12 +6,10 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-#
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -20,7 +18,8 @@
 here=`pwd`
 cd `dirname $0`/..
 srcdir=`pwd`
-cd "$here"
+mkdir -p build/release
+cd build/release
 cmake -DCMAKE_BUILD_TYPE=Release "$srcdir"
 make
 ctest .
