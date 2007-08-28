69b86f735cd0efe9c376631776a22bb120a7253c
phnixwxz <phnixwxz@1bbdc759-f934-0410-94f1-a90c52dc07f7>
2007-08-28 02:03:38 +0000

    1. Move bin and test wrappers from util directory to cmake directory;  2. Change naming convention in cmake files:    a) all parameters are in lowercase and starting with '_';    b) all local variables are composed with the macro name, '_' and lowercase name.  3. Add PkgConfigEx.cmake and modify ggadget/graphics/CMakeLists.txt to use it.

--- trunk/cmake/ZipUtils.cmake
+++ trunk/cmake/ZipUtils.cmake
@@ -15,34 +15,35 @@
 #
 
 #! Make a zip file containing all the files in the directory.
-#! @param SOURCE the full path of the directory.
-#! @param ZIP_FILE the full path of the destination zip file.
-MACRO(MAKE_ZIP SOURCE ZIP_FILE)
-  GET_FILENAME_COMPONENT(ZIP_NAME ${ZIP_FILE} NAME)
-  ADD_CUSTOM_TARGET(${ZIP_NAME}_zip ALL
-    DEPENDS ${ZIP_FILE})
-  FILE(GLOB_RECURSE FILES ${SOURCE}/*)
-  SET(RELATIVE_FILES)
-  SET(SOURCE_FILES)
-  FOREACH(FILENAME ${FILES})
-    IF(NOT ${FILENAME} MATCHES /\\.svn/)
-      FILE(RELATIVE_PATH RELATIVE_NAME ${SOURCE} ${FILENAME})
-      SET(SOURCE_FILES ${SOURCE_FILES} ${FILENAME})
-      SET(RELATIVE_FILES ${RELATIVE_FILES} ${RELATIVE_NAME})
-    ENDIF(NOT ${FILENAME} MATCHES /\\.svn/)
-  ENDFOREACH(FILENAME ${FILES})
-  ADD_CUSTOM_COMMAND(OUTPUT ${ZIP_FILE}
-    COMMAND zip -r -u ${ZIP_FILE} ${RELATIVE_FILES}
-    DEPENDS ${SOURCE_FILES}
-    WORKING_DIRECTORY ${SOURCE})
-ENDMACRO(MAKE_ZIP SOURCE ZIP_FILE)
+#! @param _source the full path of the directory.
+#! @param _zip_file the full path of the destination zip file.
+MACRO(MAKE_ZIP _source _zip_file)
+  GET_FILENAME_COMPONENT(MAKE_ZIP_name ${_zip_file} NAME)
+  ADD_CUSTOM_TARGET(${MAKE_ZIP_name}_zip ALL
+    DEPENDS ${_zip_file})
+  FILE(GLOB_RECURSE MAKE_ZIP_files ${_source}/*)
+  SET(MAKE_ZIP_relative_files)
+  SET(MAKE_ZIP_source_files)
+  FOREACH(MAKE_ZIP_filename ${MAKE_ZIP_files})
+    IF(NOT ${MAKE_ZIP_filename} MATCHES /\\.svn/)
+      FILE(RELATIVE_PATH MAKE_ZIP_relative_name ${_source} ${MAKE_ZIP_filename})
+      SET(MAKE_ZIP_source_files ${MAKE_ZIP_source_files} ${MAKE_ZIP_filename})
+      SET(MAKE_ZIP_relative_files
+        ${MAKE_ZIP_relative_files} ${MAKE_ZIP_relative_name})
+    ENDIF(NOT ${MAKE_ZIP_filename} MATCHES /\\.svn/)
+  ENDFOREACH(MAKE_ZIP_filename ${MAKE_ZIP_files})
+  ADD_CUSTOM_COMMAND(OUTPUT ${_zip_file}
+    COMMAND zip -r -u ${_zip_file} ${MAKE_ZIP_relative_files}
+    DEPENDS ${MAKE_ZIP_source_files}
+    WORKING_DIRECTORY ${_source})
+ENDMACRO(MAKE_ZIP _source _zip_file)
 
-MACRO(OUTPUT_GG DIR_NAME)
-  MAKE_ZIP(${CMAKE_CURRENT_SOURCE_DIR}/${DIR_NAME}
-    ${CMAKE_BINARY_DIR}/output/${DIR_NAME}.gg)
-ENDMACRO(OUTPUT_GG DIR_NAME)
+MACRO(OUTPUT_GG _dir_name)
+  MAKE_ZIP(${CMAKE_CURRENT_SOURCE_DIR}/${_dir_name}
+    ${CMAKE_BINARY_DIR}/output/${_dir_name}.gg)
+ENDMACRO(OUTPUT_GG _dir_name)
 
-MACRO(TEST_RESOURCE_GG DIR_NAME)
-  MAKE_ZIP(${CMAKE_CURRENT_SOURCE_DIR}/${DIR_NAME}
-    ${CMAKE_CURRENT_BINARY_DIR}/${DIR_NAME}.gg)
-ENDMACRO(TEST_RESOURCE_GG DIR_NAME)
+MACRO(TEST_RESOURCE_GG _dir_name)
+  MAKE_ZIP(${CMAKE_CURRENT_SOURCE_DIR}/${_dir_name}
+    ${CMAKE_CURRENT_BINARY_DIR}/${_dir_name}.gg)
+ENDMACRO(TEST_RESOURCE_GG _dir_name)
