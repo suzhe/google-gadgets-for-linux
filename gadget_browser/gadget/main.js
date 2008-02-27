4dd7242fef92fce9229a411be0d34d8806fabd5a
phnixwxz <phnixwxz@1bbdc759-f934-0410-94f1-a90c52dc07f7>
2008-02-27 09:00:21 +0000

    r765@wangxianzhu:  wangxianzhu | 2008-02-27 14:31:49 +0800  Added gadget downloading code.
     Reviewer: hugesea.dev

--- gadget_browser/gadget/main.js
+++ gadget_browser/gadget/main.js
@@ -16,6 +16,7 @@
 
 function view_onOpen() {
   LoadMetadata();
+  UpdateLanguageBox();
 }
 
 function language_label_onsize() {
@@ -58,56 +59,6 @@ function language_box_onchange() {
   SelectLanguage(language_box.children.item(language_box.selectedIndex).name);
 }
 
-var kPluginPrefix = "http://desktop.google.com";
-
-// Plugin updated in recent two months are listed in the "new" category.
-var kNewPluginPeriod = 2 * 30 * 86400000;
-var kIGoogleFeedModuleId = 25;
-
-var kSupportedLanguages = [
-  "bg", "ca", "cs", "da", "de", "el", "en", "en-gb", "es", "fi", "fr", "hi",
-  "hr", "hu", "id", "it", "ja", "ko", "nl", "no", "pl", "pt-br", "pt-pt",
-  "ro", "ru", "sk", "sl", "sv", "th", "tr", "zh-cn", "zh-tw",
-];
-
-var kPluginTypeSearch = "search";
-var kPluginTypeSidebar = "sidebar";
-var kPluginTypeIGoogle = "igoogle";
-var kPluginTypeFeed = "feed";
-
-var kCategoryAll = "all";
-var kCategoryNew = "new";
-var kCategoryRecommendations = "recommendations";
-var kCategoryGoogle = "google";
-var kCategoryRecentlyUsed = "recently_used";
-var kCategoryUpdates = "updates";
-
-var kCategoryNews = "news";
-var kCategorySports = "sports";
-var kCategoryLifestyle = "lifestyle";
-var kCategoryTools = "tools";
-var kCategoryFinance = "finance";
-var kCategoryFunGames = "fun_games";
-var kCategoryTechnology = "technology";
-var kCategoryCommunication = "communication";
-var kCategoryHoliday = "holiday";
-
-var kTopCategories = [
-  kCategoryAll, kCategoryNew, kCategoryRecommendations, kCategoryGoogle,
-  kCategoryRecentlyUsed, kCategoryUpdates,
-];
-
-var kBottomCategories = [
-  kCategoryNews, kCategorySports, kCategoryLifestyle, kCategoryTools,
-  kCategoryFinance, kCategoryFunGames, kCategoryTechnology,
-  kCategoryCommunication, kCategoryHoliday,
-];
-
-// gPlugins is a two-level table. It is first indexed with language names,
-// then with category names. The elements of the table are arrays of plugins.
-// For example, gPlugins may contain the following value:
-// {en:{news:[plugin1,plugin2,...],...}}.
-var gPlugins = {};
 var gCurrentCategory = "";
 var gCurrentLanguage = "";
 var gCurrentPage = 0;
@@ -122,6 +73,12 @@ var kPluginRows = 3;
 var kPluginColumns = 4;
 var kPluginsPerPage = kPluginRows * kPluginColumns;
 
+// Plugin download status.
+var kDownloadStatusNone = 0;
+var kDownloadStatusAdding = 1;
+var kDownloadStatusAdded = 2;
+var kDownloadStatusError = 3;
+
 // lang should be in kSupportedLanguages or equal to "others".
 function GetDisplayLanguage(language) {
   return strings["LANGUAGE_" + language.replace("-", "_").toUpperCase()];
@@ -130,62 +87,6 @@ function GetDisplayLanguage(language) {
 // category should be in kCategories, or "google" or "others".
 function GetDisplayCategory(category) {
   return strings["CATEGORY_" + category.toUpperCase()];
-}
-
-function SplitValues(src) {
-  return src ? src.split(",") : [];
-}
-
-function LoadMetadata() {
-  debug.trace("begin loading metadata");
-  var metadata = gadgetBrowserUtils.gadgetMetadata;
-  gPlugins = {};
-  var currentTime = new Date().getTime();
-  for (var i = 0; i < metadata.length; i++) {
-    var plugin = metadata[i];
-    var attrs = plugin.attributes;
-    if ((attrs.module_id != kIGoogleFeedModuleId ||
-         attrs.type != kPluginTypeIGoogle) &&
-        attrs.sidebar == "false") {
-      // This plugin is not for desktop, ignore it.
-      // IGoogle feeding gadgets are supported even if marked sidebar="false".
-      continue;
-    }
-
-    var languages = SplitValues(attrs.language);
-    if (languages.length == 0)
-      languages.push("en");
-    var categories = SplitValues(attrs.category);
-    // TODO: other special categories.
-    plugin.rank = parseFloat(attrs.rank);
-
-    for (var lang_i in languages) {
-      var language = languages[lang_i];
-      var language_categories = gPlugins[language];
-      if (!language_categories) {
-        language_categories = gPlugins[language] = {};
-        language_categories[kCategoryAll] = [];
-        language_categories[kCategoryNew] = [];
-        language_categories[kCategoryRecentlyUsed] = [];
-        language_categories[kCategoryUpdates] = [];
-        // TODO: recommendations.
-      }
-
-      language_categories[kCategoryAll].push(plugin);
-      if (currentTime - plugin.updated_date.getTime() < kNewPluginPeriod)
-        language_categories[kCategoryNew].push(plugin);
-
-      for (var cate_i in categories) {
-        var category = categories[cate_i];
-        var category_plugins = language_categories[category];
-        if (!category_plugins)
-          category_plugins = language_categories[category] = [];
-        category_plugins.push(plugin);
-      }
-    }
-  }
-  debug.trace("finished loading metadata");
-  UpdateLanguageBox();
 }
 
 function UpdateLanguageBox() {
@@ -264,66 +165,26 @@ function SelectCategory(category) {
   }
 }
 
-function GetPluginTitle(plugin) {
-  if (!plugin) return "";
-  var result = plugin.titles[gCurrentLanguage];
-  return result ? result : plugin.attributes.name;
-}
-
-function GetPluginDescription(plugin) {
-  if (!plugin) return "";
-  var result = plugin.descriptions[gCurrentLanguage];
-  return result ? result : plugin.attributes.product_summary;
-}
-
-function GetPluginOtherData(plugin) {
-  if (!plugin) return "";
-  var result = "";
-  var attrs = plugin.attributes;
-  if (attrs.version) {
-    result += strings.PLUGIN_VERSION;
-    result += attrs.version;
-  }
-  if (attrs.size_kilobytes) {
-    if (result) result += strings.PLUGIN_DATA_SEPARATOR;
-    result += attrs.size_kilobytes;
-    result += strings.PLUGIN_KILOBYTES;
-  }
-  if (plugin.updated_date) {
-    if (result) result += strings.PLUGIN_DATA_SEPARATOR;
-    result += plugin.updated_date.toLocaleDateString();
-  }
-  if (attrs.author) {
-    if (result) result += strings.PLUGIN_DATA_SEPARATOR;
-    result += attrs.author;
-  }
-  if (result)
-    result = strings.PLUGIN_DATA_BULLET + result;
-  // For debug:
-  // result += "/" + attrs.uuid;
-  // result += "/" + attrs.id;
-  // result += "/" + attrs.rank;
-  return result;
-}
-
 function AddPluginBox(plugin, index, row, column) {
   var box = plugins_div.appendElement(
     "<div x='" + (column * kPluginWidth) + "' y='" + (row * kPluginHeight) +
     "' width='" + kPluginWidth + "' height='" + kPluginHeight +
     "' enabled='true' backgroundMode='stretchMiddle'" +
-    " onmouseover='pluginbox_onmouseover(" + index + ")' onmouseout='pluginbox_onmouseout()'>" +
+    " onmouseover='pluginbox_onmouseover(" + index + ")'" +
+    " onmouseout='pluginbox_onmouseout(" + index + ")'>" +
     " <label x='7' y='6' width='120' align='center' color='#FFFFFF' trimming='character-ellipsis'/>" +
     " <img x='16' y='75' opacity='70' src='images/thumbnails_shadow.png'/>" +
     " <div x='27' y='33' width='80' height='83' background='#FFFFFF'>" +
-    "  <img width='80' height='60' src='images/default_thumbnail.jpg'/>" +
-    "  <img y='60' width='80' height='60' src='images/default_thumbnail.jpg' flip='vertical'/>" +
+    "  <img width='80' height='60' src='images/default_thumbnail.jpg' cropMaintainAspect='true'/>" +
+    "  <img y='60' width='80' height='60' src='images/default_thumbnail.jpg' flip='vertical' cropMaintainAspect='true'/>" +
     "  <img src='images/thumbnails_default_mask.png'/>" +
     " </div>" +
-    " <button x='22' y='94' width='90' height='30' visible='false' stretchMiddle='true'" +
-    "  image='images/add_button_default.png' downImage='images/add_button_down.png'" +
-    "  overImage='images/add_button_hover.png'" +
-    "  onmouseout='add_button_onmouseout()' onclick='AddPlugin(" + index + ")'" +
-    "  color='#FFFFFF' caption='" + strings.ADD_BUTTON_LABEL + "'/>" +
+    " <button x='22' y='94' width='90' height='30' visible='false' color='#FFFFFF'" +
+    "  stretchMiddle='true' trimming='character-ellipsis'" +
+    "  downImage='images/add_button_down.png' overImage='images/add_button_hover.png'" +
+    "  onmouseover='add_button_onmouseover(" + index + ")'" +
+    "  onmouseout='add_button_onmouseout(" + index + ")'" +
+    "  onclick='add_button_onclick(" + index + ")'/>" +
     "</div>");
 
   // Set it here to prevent problems caused by special chars in the title.
@@ -332,14 +193,106 @@ function AddPluginBox(plugin, index, row, column) {
   var thumbnail_element1 = box.children.item(2).children.item(0);
   var thumbnail_element2 = box.children.item(2).children.item(1);
   AddThumbnailTask(plugin, thumbnail_element1, thumbnail_element2);
+
+  plugin.button = box.children.item(3);
+  UpdateAddButtonVisualStatus(plugin.button, plugin.download_status);
 }
 
-function AddPlugin(index) {
-  var real_index = gCurrentPage * kPluginsPerPage + index;
+function SetDownloadStatus(plugin, status) {
+  plugin.download_status = status;
+  var button = plugin.button;
+  if (!button)
+    return;
+
+  try {
+    button.enabled = button.enabled;
+  } catch (e) {
+    plugin.button = null;
+    // The button has been destroyed, so don't update its visual status.
+    return;
+  }
+  UpdateAddButtonVisualStatus(button, status);
+}
+
+var kAddingStatusLabels = [
+  strings.ADD_BUTTON_LABEL,
+  strings.STATUS_ADDING,
+  strings.STATUS_ADDED,
+  strings.STATUS_ERROR_ADDING,
+];
+var kUpdatingStatusLabels = [
+  strings.UPDATE_BUTTON_LABEL,
+  strings.STATUS_UPDATING,
+  strings.STATUS_UPDATED,
+  strings.STATUS_ERROR_UPDATING,
+];
+var kStatusButtonImages = [
+  "images/add_button_default.png",
+  "images/status_adding_default.png",
+  "images/status_added_default.png",
+  "images/status_failed_default.png",
+];
+  
+function UpdateAddButtonVisualStatus(button, status) {
+  var labels = gCurrentCategory == kCategoryUpdates ?
+               kUpdatingStatusLabels : kAddingStatusLabels;
+  button.caption = labels[status];
+  button.image = kStatusButtonImages[status];
+  button.enabled = status != kDownloadStatusAdding;
+
+  if (status == kDownloadStatusNone) {
+    button.overImage = "images/add_button_hover.png";
+  } else {
+    button.visible = true;
+    button.overImage = kStatusButtonImages[status];
+  }
+}
+
+function add_button_onclick(index) {
+  var plugin = gCurrentPlugins[index];
+  plugin.button = event.srcElement;
+  SetDownloadStatus(plugin, kDownloadStatusAdding);
+  if (gadgetBrowserUtils.needDownloadGadget(plugin.id)) {
+    // TODO: non-binary plugin, whose download_url is url to a webpage.
+    DownloadPlugin(plugin);
+  } else {
+    switch (gadgetBrowserUtils.addPlugin(plugin.id)) {
+      case -1:
+        SetDownloadStatus(plugin, kDownloadStatusError);
+        break;
+      case 1:
+        SetDownloadStatus(plugin, kDownloadStatusAdded);
+        break;
+      default:
+        SetDownloadStatus(plugin, kDownloadStatusNone);
+        break;
+    }
+  }
 }
 
 function pluginbox_onmouseover(index) {
   MouseOverPlugin(event.srcElement, index);
+}
+
+function pluginbox_onmouseout(index) {
+  var box = event.srcElement;
+  // Exclude the case that the mouse moved over the "Add" button.
+  if (!view.mouseOverElement || view.mouseOverElement.parentElement != box)
+    MouseOutPlugin(box, index);
+}
+
+function add_button_onmouseover(index) {
+  // Won't change status when adding, because the button is disabled in that
+  // status and won't fire onmouseover.
+  SetDownloadStatus(gCurrentPlugins[index], kDownloadStatusNone);
+  MouseOverPlugin(event.srcElement.parentElement, index);
+}
+
+function add_button_onmouseout(index) {
+  var box = event.srcElement.parentElement;
+  // The mouse may directly moved out of the pluginbox.
+  if (!view.mouseOverElement || view.mouseOverElement != box)
+    MouseOutPlugin(box, index);
 }
 
 function MouseOverPlugin(box, index) {
@@ -355,25 +308,12 @@ function MouseOverPlugin(box, index) {
   plugin_other_data.innerText = GetPluginOtherData(plugin);
 }
 
-function pluginbox_onmouseout() {
-  var box = event.srcElement;
-  // Exclude the case that the mouse moved over the "Add" button.
-  if (!view.mouseOverElement || view.mouseOverElement.parentElement != box)
-    MouseOutPlugin(box);
-}
-
-function add_button_onmouseout() {
-  var box = event.srcElement.parentElement;
-  // The mouse may directly moved out of the pluginbox.
-  if (!view.mouseOverElement || view.mouseOverElement != box)
-    MouseOutPlugin(box);
-}
-
-function MouseOutPlugin(box) {
+function MouseOutPlugin(box, index) {
   box.background = "";
   box.children.item(2).children.item(2).src = "images/thumbnails_default_mask.png";
-  // Hide the "Add" button.
-  box.children.item(3).visible = false;
+  // Hide the "Add" button when it's in normal state.
+  if (!gCurrentPlugins[index].download_status)
+    box.children.item(3).visible = false;
   plugin_title.innerText = "";
   plugin_description.innerText = "";
   plugin_other_data.innerText = "";
@@ -474,45 +414,13 @@ function search_box_onchange() {
     // Undo the actions in ResetSearchBox() called by SelectCategory().
     search_box_onfocusin();
   } else {
-    setTimeout(DoSearch, kSearchDelay);
-  }
-}
-
-function DoSearch() {
-  if (!search_box.value || search_box.value == strings.SEARCH_GADGETS)
-    return;
-
-  debug.trace("search begins");
-  var terms = search_box.value.toUpperCase().split(" ");
-  var plugins = gPlugins[gCurrentLanguage][kCategoryAll];
-  var result = [];
-  if (plugins && plugins.length) {
-    for (var i = 0; i < plugins.length; i++) {
-      var plugin = plugins[i];
-      var attrs = plugin.attributes;
-      var indexable_text = [
-        attrs.author ? attrs.author : "",
-        plugin.updated_date ? plugin.updated_date.toLocaleDateString() : "",
-        attrs.download_url ? attrs.download_url : "",
-        attrs.info_url ? attrs.info_url : "",
-        attrs.keywords ? attrs.keywords : "",
-        GetPluginTitle(plugin),
-        GetPluginDescription(plugin),
-      ].join(" ").toUpperCase();
-
-      var ok = true;
-      for (var j in terms) {
-        if (terms[j] && indexable_text.indexOf(terms[j]) == -1) {
-          ok = false;
-          break;
-        }
+    setTimeout(function () {
+      if (search_box.value && search_box.value != strings.SEARCH_GADGETS) {
+        SelectCategory(null);
+        gCurrentPlugins = SearchPlugins(search_box.value);
+        SelectPage(0);
       }
-      if (ok) result.push(plugin);
-    }
+    }, kSearchDelay);
   }
-
-  SelectCategory(null);
-  gCurrentPlugins = result;
-  SelectPage(0);
-  debug.trace("search ends");
 }
+
---
+++ gadget_browser/gadget/plugins.js
@@ -0,0 +1,247 @@
+/*
+  Copyright 2007 Google Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+var kPluginDownloadURLPrefix = "http://desktop.google.com";
+
+// gPlugins is a two-level table. It is first indexed with language names,
+// then with category names. The elements of the table are arrays of plugins.
+// For example, gPlugins may contain the following value:
+// {en:{news:[plugin1,plugin2,...],...}}.
+var gPlugins = {};
+var gPluginIdIndex = {};
+
+// Plugin updated in recent two months are listed in the "new" category.
+var kNewPluginPeriod = 2 * 30 * 86400000;
+var kIGoogleFeedModuleId = 25;
+
+var kSupportedLanguages = [
+  "bg", "ca", "cs", "da", "de", "el", "en", "en-gb", "es", "fi", "fr", "hi",
+  "hr", "hu", "id", "it", "ja", "ko", "nl", "no", "pl", "pt-br", "pt-pt",
+  "ro", "ru", "sk", "sl", "sv", "th", "tr", "zh-cn", "zh-tw",
+];
+
+var kPluginTypeSearch = "search";
+var kPluginTypeSidebar = "sidebar";
+var kPluginTypeIGoogle = "igoogle";
+var kPluginTypeFeed = "feed";
+
+var kCategoryAll = "all";
+var kCategoryNew = "new";
+var kCategoryRecommendations = "recommendations";
+var kCategoryGoogle = "google";
+var kCategoryRecentlyUsed = "recently_used";
+var kCategoryUpdates = "updates";
+
+var kCategoryNews = "news";
+var kCategorySports = "sports";
+var kCategoryLifestyle = "lifestyle";
+var kCategoryTools = "tools";
+var kCategoryFinance = "finance";
+var kCategoryFunGames = "fun_games";
+var kCategoryTechnology = "technology";
+var kCategoryCommunication = "communication";
+var kCategoryHoliday = "holiday";
+
+var kTopCategories = [
+  kCategoryAll, kCategoryNew, kCategoryRecommendations, kCategoryGoogle,
+  kCategoryRecentlyUsed, kCategoryUpdates,
+];
+
+var kBottomCategories = [
+  kCategoryNews, kCategorySports, kCategoryLifestyle, kCategoryTools,
+  kCategoryFinance, kCategoryFunGames, kCategoryTechnology,
+  kCategoryCommunication, kCategoryHoliday,
+];
+
+function SplitValues(src) {
+  return src ? src.split(",") : [];
+}
+
+function LoadMetadata() {
+  debug.trace("begin loading metadata");
+  var metadata = gadgetBrowserUtils.gadgetMetadata;
+  gPlugins = {};
+  var currentTime = new Date().getTime();
+  for (var i = 0; i < metadata.length; i++) {
+    var plugin = metadata[i];
+    gPluginIdIndex[plugin.id] = plugin;
+    plugin.download_status = kDownloadStatusNone;
+
+    var attrs = plugin.attributes;
+    if ((attrs.module_id != kIGoogleFeedModuleId ||
+         attrs.type != kPluginTypeIGoogle) &&
+        attrs.sidebar == "false") {
+      // This plugin is not for desktop, ignore it.
+      // IGoogle feeding gadgets are supported even if marked sidebar="false".
+      continue;
+    }
+
+    var languages = SplitValues(attrs.language);
+    if (languages.length == 0)
+      languages.push("en");
+    var categories = SplitValues(attrs.category);
+    // TODO: other special categories.
+    plugin.rank = parseFloat(attrs.rank);
+
+    for (var lang_i in languages) {
+      var language = languages[lang_i];
+      var language_categories = gPlugins[language];
+      if (!language_categories) {
+        language_categories = gPlugins[language] = {};
+        language_categories[kCategoryAll] = [];
+        language_categories[kCategoryNew] = [];
+        language_categories[kCategoryRecentlyUsed] = [];
+        language_categories[kCategoryUpdates] = [];
+        // TODO: recommendations.
+      }
+
+      language_categories[kCategoryAll].push(plugin);
+      if (currentTime - plugin.updated_date.getTime() < kNewPluginPeriod)
+        language_categories[kCategoryNew].push(plugin);
+
+      for (var cate_i in categories) {
+        var category = categories[cate_i];
+        var category_plugins = language_categories[category];
+        if (!category_plugins)
+          category_plugins = language_categories[category] = [];
+        category_plugins.push(plugin);
+      }
+    }
+  }
+  debug.trace("finished loading metadata");
+}
+
+function GetPluginTitle(plugin) {
+  if (!plugin) return "";
+  var result = plugin.titles[gCurrentLanguage];
+  return result ? result : plugin.attributes.name;
+}
+
+function GetPluginDescription(plugin) {
+  if (!plugin) return "";
+  var result = plugin.descriptions[gCurrentLanguage];
+  return result ? result : plugin.attributes.product_summary;
+}
+
+function GetPluginOtherData(plugin) {
+  if (!plugin) return "";
+  var result = "";
+  var attrs = plugin.attributes;
+  if (attrs.version) {
+    result += strings.PLUGIN_VERSION;
+    result += attrs.version;
+  }
+  if (attrs.size_kilobytes) {
+    if (result) result += strings.PLUGIN_DATA_SEPARATOR;
+    result += attrs.size_kilobytes;
+    result += strings.PLUGIN_KILOBYTES;
+  }
+  if (plugin.updated_date.getTime()) {
+    if (result) result += strings.PLUGIN_DATA_SEPARATOR;
+    result += plugin.updated_date.toLocaleDateString();
+  }
+  if (attrs.author) {
+    if (result) result += strings.PLUGIN_DATA_SEPARATOR;
+    result += attrs.author;
+  }
+  if (result)
+    result = strings.PLUGIN_DATA_BULLET + result;
+  // For debug:
+  // result += "/" + attrs.uuid;
+  // result += "/" + attrs.id;
+  // result += "/" + attrs.rank;
+  return result;
+}
+
+function DownloadPlugin(plugin, is_updating) {
+  var download_url = plugin.attributes.download_url;
+  if (!download_url) {
+    SetDownloadStatus(plugin, kDownloadStatusError);
+    return;
+  }
+
+  if (!download_url.match(/^https?:\/\//))
+    download_url = kPluginDownloadURLPrefix + download_url;
+  gadget.debug.trace("Start downloading gadget: " + download_url);
+
+  var request = new XMLHttpRequest();
+  try {
+    request.open("GET", download_url);
+    request.onreadystatechange = function() {
+      OnRequestStateChange();
+    };
+    request.send();
+  } catch (e) {
+    gadget.debug.error("Request error: " + e);
+  }
+
+  function OnRequestStateChange() {
+    if (request.readyState == 4) {
+      if (request.status == 200) {
+        gadget.debug.trace("Finished downloading a gadget: " + download_url);
+        // gPlugins may have been updated during the download, so re-get the
+        // plugin with its id.
+        plugin = gPluginIdIndex[plugin.id];
+        if (plugin) {
+          gadgetBrowserUtils.saveGadget(plugin.id, request.responseBody);
+          if (is_updating)
+            gadgetBrowserUtils.updateGadget(plugin.id);
+          else
+            gadgetBrowserUtils.addGadget(plugin.id);
+          SetDownloadStatus(plugin, kDownloadStatusAdded);
+        }
+      } else {
+        gadget.debug.error("Download request " + download_url +
+                           " returned status: " + request.status);
+        SetDownloadStatus(plugin, kDownloadStatusError);
+      }
+    }
+  }
+}
+
+function SearchPlugins(search_string) {
+  debug.trace("search begins");
+  var terms = search_string.toUpperCase().split(" ");
+  var plugins = gPlugins[gCurrentLanguage][kCategoryAll];
+  var result = [];
+  if (plugins && plugins.length) {
+    for (var i = 0; i < plugins.length; i++) {
+      var plugin = plugins[i];
+      var attrs = plugin.attributes;
+      var indexable_text = [
+        attrs.author ? attrs.author : "",
+        plugin.updated_date ? plugin.updated_date.toLocaleDateString() : "",
+        attrs.download_url ? attrs.download_url : "",
+        attrs.info_url ? attrs.info_url : "",
+        attrs.keywords ? attrs.keywords : "",
+        GetPluginTitle(plugin),
+        GetPluginDescription(plugin),
+      ].join(" ").toUpperCase();
+
+      var ok = true;
+      for (var j in terms) {
+        if (terms[j] && indexable_text.indexOf(terms[j]) == -1) {
+          ok = false;
+          break;
+        }
+      }
+      if (ok) result.push(plugin);
+    }
+  }
+
+  debug.trace("search ends");
+  return result;
+}
