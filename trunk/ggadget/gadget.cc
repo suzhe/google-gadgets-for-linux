4ff9da0e8dc3e5612f18e35a3d3790c0b8533b31
james.su <james.su@1bbdc759-f934-0410-94f1-a90c52dc07f7>
2008-03-18 12:01:25 +0000

    - Fix font and object loading bug (again).  - Rename gadget-browser to google-gadget-browser.  - Add configure options to enable/disable each components individually.  - Rename ggl-resources.gg to resources.gg  - A small fix to google_gadget_manager/Makefile.am.

--- trunk/ggadget/gadget.cc
+++ trunk/ggadget/gadget.cc
@@ -172,6 +172,20 @@ class Gadget::Impl : public ScriptableHelperNativeOwnedDefault {
     element_factory_ = NULL;
   }
 
+  static bool ExtractFileFromFileManager(FileManagerInterface *fm,
+                                         const char *file,
+                                         std::string *path) {
+    path->clear();
+    if (fm->IsDirectlyAccessible(file, path))
+      return true;
+
+    path->clear();
+    if (fm->ExtractFile(file, path))
+      return true;
+
+    return false;
+  }
+
   // Do real initialize.
   bool Initialize() {
     if (!host_ || !element_factory_ || !file_manager_ || !options_ ||
@@ -219,15 +233,13 @@ class Gadget::Impl : public ScriptableHelperNativeOwnedDefault {
         const char *font_name = i->second.c_str();
         std::string path;
         // ignore return, error not fatal
-        if (file_manager_->IsDirectlyAccessible(font_name, &path) ||
-            file_manager_->ExtractFile(font_name, &path))
+        if (ExtractFileFromFileManager(file_manager_, font_name, &path))
           host_->LoadFont(path.c_str());
       } else if (SimpleMatchXPath(key.c_str(), kManifestInstallObjectSrc) &&
                  extension_manager_) {
         const char *module_name = i->second.c_str();
         std::string path;
-        if (file_manager_->IsDirectlyAccessible(module_name, &path) ||
-            file_manager_->ExtractFile(module_name, &path))
+        if (ExtractFileFromFileManager(file_manager_, module_name, &path))
           extension_manager_->LoadExtension(path.c_str(), false);
       }
     }
