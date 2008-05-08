62d0b40f8f505b9e3bb4ce80991108b13f8e2b0e
james.su <james.su@1bbdc759-f934-0410-94f1-a90c52dc07f7>
2008-05-08 11:31:48 +0000

    Remove unused branches.

--- changes/james.su/s5x3/trunk/cmake/test_long_int64_t.c
+++
@@ -1,8 +0,0 @@
-// This file is used to test if long and int64_t are the same type.
-// It will fail to compile if long and int64_t are not the same type.
-#include <stdint.h>
-int main() {
-  long f(long a);
-  int64_t f(int64_t a);
-  return 0;
-}
